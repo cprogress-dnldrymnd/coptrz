4ee327cb39085d32a20aeb2086021102643c38d9
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-06 01:55:28 +0800

    Update related.php

--- template-parts/single/single-post/related.php
+++ template-parts/single/single-post/related.php
@@ -17,10 +17,10 @@ $categories = get_the_category(get_the_ID());
         </div>
     </div>
     <div class="container">
-        
-                <?php
-                var_dump($categories);
-                ?>
+
+        <?php
+        var_dump($categories);
+        ?>
         <div class="product-slider-box">
             <div class="swiper mySwiper-productSwiper mySwiper-productSwiper-medium mySwiper-PostSlider">
                 <div class="swiper-wrapper product-holder post-box-PostSlider align-items-stretch">
@@ -32,62 +32,63 @@ $categories = get_the_category(get_the_ID());
                         'post_type'      => array('post'),
 
                         'post_status'    => 'publish',
-                        
+
 
                     );
                     $query = new WP_Query($args);
                     ?>
+                    <?php if ($query->have_posts()) { ?>
 
+                        <?php while ($products->have_posts()) { ?>
+                            <?php
+                            $post_id = get_the_ID();
+                            ?>
+                            <div class="swiper-slide product-box">
+                                <div class="inner background-white d-block ">
+                                    <?php
+                                    $DisplayData->image(
+                                        array(
+                                            'image_id'    => get_post_thumbnail_id($post_id),
+                                            'size'        => 'medium',
+                                            'placeholder' => true
+                                        ),
+                                        'position-relative image-cover-transform'
+                                    );
+                                    ?>
 
-                    <?php foreach ($suggested_articles as $article) { ?>
-                        <?php
-                        $post_id = $article['id'];
-                        ?>
-                        <div class="swiper-slide product-box">
-                            <div class="inner background-white d-block ">
-                                <?php
-                                $DisplayData->image(
-                                    array(
-                                        'image_id'    => get_post_thumbnail_id($post_id),
-                                        'size'        => 'medium',
-                                        'placeholder' => true
-                                    ),
-                                    'position-relative image-cover-transform'
-                                );
-                                ?>
+                                    <?php
+                                    $DisplayData->heading(
+                                        array(
+                                            'heading' => get_the_title($post_id),
+                                            'tag'     => 'h4'
+                                        )
+                                    );
+                                    ?>
+                                    <div class="bottom-box">
+                                        <div class="meta-box d-flex">
+                                            <span class="date">
+                                                <?= get_the_date('m/d/Y', $post_id) ?>
+                                            </span>
+                                            <div class="bull">&bull;</div>
+                                            <span class="author">
+                                                <?php
+                                                $author_id = get_post_field('post_author', $post_id);
+                                                $author_name = get_the_author_meta('display_name', $author_id);
+                                                ?>
+                                                <?= $author_name ?>
+                                            </span>
+                                        </div>
 
-                                <?php
-                                $DisplayData->heading(
-                                    array(
-                                        'heading' => get_the_title($post_id),
-                                        'tag'     => 'h4'
-                                    )
-                                );
-                                ?>
-                                <div class="bottom-box">
-                                    <div class="meta-box d-flex">
-                                        <span class="date">
-                                            <?= get_the_date('m/d/Y', $post_id) ?>
-                                        </span>
-                                        <div class="bull">&bull;</div>
-                                        <span class="author">
-                                            <?php
-                                            $author_id = get_post_field('post_author', $post_id);
-                                            $author_name = get_the_author_meta('display_name', $author_id);
-                                            ?>
-                                            <?= $author_name ?>
-                                        </span>
+                                        <div class="link-box">
+                                            <a href="<?= get_permalink($post_id) ?>" class="link-underline fw-medium">Read
+                                                more</a>
+                                        </div>
                                     </div>
 
-                                    <div class="link-box">
-                                        <a href="<?= get_permalink($post_id) ?>" class="link-underline fw-medium">Read
-                                            more</a>
-                                    </div>
                                 </div>
-
                             </div>
-                        </div>
 
+                        <?php } ?>
                     <?php } ?>
                 </div>
                 <div class="swiper-nav-holder d-none d-sm-inline-flex">
