920ec0ee8eb0203340ec788265d3a43e59c3ebac
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-06 02:32:55 +0800

    theme options

--- includes/theme-options.php
+++ includes/theme-options.php
@@ -4,6 +4,7 @@ class Theme_Options extends Helpers
 	function __construct()
 	{
 		$SVG = new SVG;
+		$DisplayData = new DisplayData;
 		$this->site_logo = wp_get_attachment_image_url(get__theme_option('logo'), 'large');
 		$this->alt_logo_url = wp_get_attachment_image_url(get__theme_option('alt_logo'), 'large');
 		$this->disable_gutenberg = get__theme_option('disable_gutenberg');
@@ -13,7 +14,7 @@ class Theme_Options extends Helpers
 
 
 
-		$this->logo = '<a class="site-logo full-logo" href="' . get_site_url() . '"> <img src="' . wp_get_attachment_image_url(get__theme_option('logo'), 'medium') . '" alt="' . get_bloginfo('name') . '"> </a>';
+		$this->logo = '<a class="site-logo full-logo" href="' . get_site_url() . '"> '.$DisplayData->image(array('image_id' => get__theme_option('logo'))).' </a>';
 		$this->alt_logo = '<a class="site-logo alt-logo" href="' . get_site_url() . '"> <img src="' . wp_get_attachment_image_url(get__theme_option('alt_logo'), 'large') . '" alt="' . get_bloginfo('name') . '"></a>';
 		$this->contact_number_text  = get__theme_option('contact_number');
 		$this->contact_number_url = 'tel:' . $this->clean_string($this->contact_number_text, '');
