93b092c1645c0a0a0c5874c0a9304c2a2a7f33b4
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-07 16:16:31 +0800

    Update _featured_product_slider.php

--- template-parts/modules/_featured_product_slider.php
+++ template-parts/modules/_featured_product_slider.php
@@ -17,9 +17,6 @@ $featured_products = $module['featured_products'];
                         <?php
                         $product = wc_get_product($featured_product['id']);
                         $vendors = $product->get_attribute('pa_vendors');
-                        echo $vendors;
-                        echo wc_attribute_taxonomy_id_by_name($vendors);
-
 
                         ?>
                         <div class="swiper-slide">
