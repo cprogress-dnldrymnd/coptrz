49a4050d510a3200d504770dd860bbc0c764e997
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-08 21:19:58 +0800

    Update _featured_product_slider.php

--- template-parts/modules/_featured_product_slider.php
+++ template-parts/modules/_featured_product_slider.php
@@ -56,11 +56,7 @@ $featured_products = $module['featured_products'];
                                             $product_id_global = $featured_product['id'];
                                             ?>
                                             <?= $GetData->add_to_cart($featured_product['id']) ?>
-                                            <?php if($product->get_price_html()) { ?>
-                                            <div class="button-box button-accent">
-                                                <a href="<?= get_permalink($featured_product['id']) ?>">VIEW PRODUCT</a>
-                                            </div>
-                                            <?php } ?>
+                                         
                                         </div>
                                     </div>
                                 </div>
