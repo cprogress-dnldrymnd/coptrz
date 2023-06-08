72afa7bc6d46e5f3b61646ceabf7c08112375166
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-08 13:22:34 +0800

    images

--- template-parts/components/image.php
+++ template-parts/components/image.php
@@ -4,10 +4,10 @@ $size = isset($data['size']) ? $data['size'] : 'full';
 $placeholder = isset($data['placeholder']) ? true : false;
 $image_url = wp_get_attachment_image_url($data['image_id'], $size);
 
-$ext = wp_check_filetype(wp_get_attachment_url($data['image_id']))['ext'];
 $img_class = '';
 if ($image_url) {
 	$image_alt = $GetData->get_image_alt($data['image_id']);
+	$image_id = $data['image_id'];
 }
 else {
 	if ($placeholder) {
@@ -15,9 +15,11 @@ else {
 		$image_url = wp_get_attachment_image_url($logo);
 		$image_alt = $GetData->get_image_alt($logo);
 		$img_class = 'placeholder-image image-contain-transform';
-
+		$image_id = $logo;
 	}
 }
+
+$ext = wp_check_filetype(wp_get_attachment_url($image_id)['ext']);
 ?>
 <?php if ($image_url || $placeholder) { ?>
 
@@ -26,7 +28,7 @@ else {
 			<img src="<?= $image_url ?>" alt="<?= $image_alt ?>">
 		<?php }
 		else { ?>
-		<span class="svg-image" src="<?= $image_url ?>" alt="<?= $image_alt ?>"></span>
+			<span class="svg-image" src="<?= $image_url ?>" alt="<?= $image_alt ?>"></span>
 		<?php } ?>
 	</div>
 <?php } ?>
