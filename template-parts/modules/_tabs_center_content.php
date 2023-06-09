bd5cf60a900c87e124cf0bccf0db48afea6b08f6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-09 20:19:23 +0800

    Update _tabs_center_content.php

--- template-parts/modules/_tabs_center_content.php
+++ template-parts/modules/_tabs_center_content.php
@@ -23,8 +23,8 @@ $items = $module['items'];
         <div class="col-12 col-lg col-left">
             <div class="column-holder py-5">
                 <div class="nav nav-tabs" role="tablist">
-                    <button class="nav-link text-start active" id="nav-1-tab" data-bs-toggle="tab"
-                        data-bs-target="#nav-1" type="button" role="tab" aria-controls="nav-1" aria-selected="true">
+                    <button class="nav-link text-start active" id="nav-0-tab" data-bs-toggle="tab"
+                        data-bs-target="#nav-0" type="button" role="tab" aria-controls="nav-0" aria-selected="true">
                         <div class="icon-heading-holder">
                             <?php
                             $DisplayData->image(
@@ -49,8 +49,8 @@ $items = $module['items'];
                             ?>
                         </div>
                     </button>
-                    <button class="nav-link text-start" id="nav-2-tab" data-bs-toggle="tab" data-bs-target="#nav-2"
-                        type="button" role="tab" aria-controls="nav-2" aria-selected="false">
+                    <button class="nav-link text-start" id="nav-1-tab" data-bs-toggle="tab" data-bs-target="#nav-1"
+                        type="button" role="tab" aria-controls="nav-1" aria-selected="false">
                         <div class="icon-heading-holder">
                             <?php
                             $DisplayData->image(
@@ -81,39 +81,22 @@ $items = $module['items'];
         <div class="col-lg-5 col-center">
             <div class="column-holder">
                 <div class="tab-content" id="tab-center-tabContent">
-                    <div class="tab-pane fade show active" id="nav-1" role="tabpanel" aria-labelledby="nav-1-tab">
-                        <div class="image-box">
-                            <img src="<?= content_url() ?>/uploads/2022/11/windmill-1.png" alt="">
-                            <img src="<?= content_url() ?>/uploads/2022/11/drone-image.png" alt="" class="drone-image">
+                    <?php foreach ($items as $key => $item) { ?>
+                        <div class="tab-pane fade show active" id="nav-<?= $key ?>" role="tabpanel" aria-labelledby="nav-0-tab">
+                            <div class="image-box">
+                                <img src="<?= wp_get_attachment_image_url( $item['image'], $item['image'] ) ?>" alt="">
+                                <img src="<?= wp_get_attachment_image_url($item['image'], $item['image_top_right']) ?>" alt="" class="drone-image">
+                            </div>
                         </div>
-                    </div>
-                    <div class="tab-pane fade" id="nav-2" role="tabpanel" aria-labelledby="nav-2-tab">
-                        <div class="image-box">
-                            <img src="<?= content_url() ?>/uploads/2022/11/windmill-1.png" alt="">
-                            <img src="<?= content_url() ?>/uploads/2022/11/drone-image.png" alt="" class="drone-image">
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="nav-3" role="tabpanel" aria-labelledby="nav-3-tab">
-                        <div class="image-box">
-                            <img src="<?= content_url() ?>/uploads/2022/11/windmill-1.png" alt="">
-                            <img src="<?= content_url() ?>/uploads/2022/11/drone-image.png" alt="" class="drone-image">
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="nav-4" role="tabpanel" aria-labelledby="nav-4-tab">
-                        <div class="image-box">
-                            <img src="<?= content_url() ?>/uploads/2022/11/windmill-1.png" alt="">
-                            <img src="<?= content_url() ?>/uploads/2022/11/drone-image.png" alt="" class="drone-image">
-                        </div>
-                    </div>
+                    <?php } ?>
                 </div>
-
             </div>
         </div>
         <div class="col-12 col-lg col-right">
             <div class="column-holder py-5">
                 <div class="nav nav-tabs" role="tablist">
-                    <button class="nav-link text-start" id="nav-3-tab" data-bs-toggle="tab" data-bs-target="#nav-3"
-                        type="button" role="tab" aria-controls="nav-3" aria-selected="false">
+                    <button class="nav-link text-start" id="nav-2-tab" data-bs-toggle="tab" data-bs-target="#nav-2"
+                        type="button" role="tab" aria-controls="nav-2" aria-selected="false">
                         <?php
                         $DisplayData->image(
                             array(
@@ -136,8 +119,8 @@ $items = $module['items'];
                         );
                         ?>
                     </button>
-                    <button class="nav-link text-start" id="nav-4-tab" data-bs-toggle="tab" data-bs-target="#nav-4"
-                        type="button" role="tab" aria-controls="nav-4" aria-selected="false">
+                    <button class="nav-link text-start" id="nav-3-tab" data-bs-toggle="tab" data-bs-target="#nav-3"
+                        type="button" role="tab" aria-controls="nav-3" aria-selected="false">
                         <?php
                         $DisplayData->image(
                             array(
