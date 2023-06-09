b81064a35b6a317dae3278548ea2e71ed4d4c79b
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-09 20:00:01 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -108,15 +108,15 @@ class PostMeta extends GetData
 				->set_classes('select-button-icon ')
 				->set_width(10),
 			/*Field::make('html',  $id . '_button_select_icon', 'Select Icon')
-																																																						->set_html('<a class="button button-primary button-large thickbox select-icon" href="#TB_inline?width=600&height=550&inlineId=modal-svg-" >SELECT ICON</a>')
-																																																						->set_conditional_logic(array(
-																																																						array(
-																																																						'field' => $id . '_button_type',
-																																																						'value' => array('page_button', 'post_button', 'services_button', 'custom_button'),
-																																																						'compare' => 'IN'
-																																																						)
-																																																						))
-																																																						->set_width(20)*/
+																																																							 ->set_html('<a class="button button-primary button-large thickbox select-icon" href="#TB_inline?width=600&height=550&inlineId=modal-svg-" >SELECT ICON</a>')
+																																																							 ->set_conditional_logic(array(
+																																																							 array(
+																																																							 'field' => $id . '_button_type',
+																																																							 'value' => array('page_button', 'post_button', 'services_button', 'custom_button'),
+																																																							 'compare' => 'IN'
+																																																							 )
+																																																							 ))
+																																																							 ->set_width(20)*/
 
 		);
 
@@ -354,6 +354,18 @@ class ModulesFields extends GetData
 					Field::make('html', 'seperator_1')->set_html('<label>CONTENTS</label>')->set_classes('seperator '),
 					Field::make('text', 'heading_prefix', 'Heading Prefix'),
 					Field::make('text', 'heading', 'Heading'),
+					Field::make('complex', 'items', 'Items')
+						->add_fields(
+							array(
+								Field::make('image', 'icon', 'Icon'),
+								Field::make('text', 'heading', 'Heading'),
+								Field::make('textarea', 'description', 'Description'),
+								Field::make('image', 'image', 'Image'),
+								Field::make('image', 'image_top_right', 'Top right image'),
+							)
+						)
+						->set_layout('tabbed-vertical')
+						->set_header_template('<%- heading  %>')
 				),
 			)
 		);
