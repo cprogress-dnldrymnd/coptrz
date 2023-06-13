732e1ce2a561b6886aaa3143a6ce056082406f28
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-13 16:12:50 +0800

    Update woocommerce.php

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -814,7 +814,7 @@ function display_filter($post_count, $display_type, $class = '', $param = '')
 
         <?php if (isset($_GET['id'])) { ?>
             <script>
-                jQuery(document).animate({
+                jQuery('body').animate({
                     scrollTop: jQuery("#<?= $_GET['id'] ?>").offset().top
                 }, 2000);
             </script>
