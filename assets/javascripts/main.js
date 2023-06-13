95828cf67e707e2f131dcb8e0c682278991e6e3c
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-13 14:17:55 +0800

    Update main.js

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -255,9 +255,9 @@ function ajax_product($product_id, $button) {
 			image_to_svg();
 
 			if (window.innerWidth > 1199) {
-				jQuery('.product-modal-footer').addClass('d-none').appendTo('#productModalDetails');
+				jQuery('.product-add-to-cart-holder').appendTo('.product-modal-footer .container');
 			} else {
-				jQuery('.product-modal-footer').removeClass('d-none').appendTo('.product-summary .col-lg-7 .column-holder');
+				jQuery('.product-add-to-cart-holder').appendTo('.product-summary .col-lg-7 .column-holder');
 			}
 
 			setTimeout(function () {
