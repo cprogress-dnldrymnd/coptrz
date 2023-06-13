cf23106815c69f6eaca30659ad06ce9e93d086ae
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-13 14:30:51 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -233,6 +233,13 @@ function product_modal_ajax()
 	<?php
 	if ($type == 'variable') {
 		$variations = $product->get_available_variations();
+		if(get_current_user_id(  ) ==1) {
+				foreach ($variations as $variation) {
+					echo '<pre>';
+					var_dump($variation);
+					echo '</pre>';
+			}
+		}
 		?>
 		<script>
 			<?php foreach ($variations as $variation) { ?>
