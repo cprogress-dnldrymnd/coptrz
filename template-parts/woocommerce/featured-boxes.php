f0d04386e74f2b47be7bcc522887caaf32800486
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-15 17:05:47 +0800

    Update featured-boxes.php

--- template-parts/woocommerce/featured-boxes.php
+++ template-parts/woocommerce/featured-boxes.php
@@ -9,13 +9,14 @@ $featured_boxes = get__term_meta($term->term_id, 'featured_boxes');
                 <div class="row">
                     <?php foreach ($featured_boxes as $featured_box) { ?>
                         <div class="col-lg-6">
+                            <?php
+                            if ($featured_box['link']) {
+                                echo '<a href="' . $featured_box['link'] . '">';
+                            }
+                            ?>
                             <div class="column-holder text-center bg-image"
                                 style="background-image: url(<?= wp_get_attachment_image_url($featured_box['image'], 'large') ?>)">
-                                <?php
-                                if ($featured_box['link']) {
-                                    echo '<a href="' . $featured_box['link'] . '">';
-                                }
-                                ?>
+
                                 <div class="heading-box">
                                     <span class="prefix"><?= $featured_box['prefix'] ?></span>
                                     <h4><?= $featured_box['heading'] ?></h4>
@@ -26,12 +27,13 @@ $featured_boxes = get__term_meta($term->term_id, 'featured_boxes');
                                         <?= $featured_box['description'] ?>
                                     </p>
                                 </div>
-                                <?php
-                                if ($featured_box['link']) {
-                                    echo '</a>';
-                                }
-                                ?>
+
                             </div>
+                            <?php
+                            if ($featured_box['link']) {
+                                echo '</a>';
+                            }
+                            ?>
                         </div>
                     <?php } ?>
                 </div>
