ca58e96df76fbc6f04b7c8f16d9b1a2bc56d491d
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-15 16:53:04 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -108,15 +108,15 @@ class PostMeta extends GetData
 				->set_classes('select-button-icon ')
 				->set_width(10),
 			/*Field::make('html',  $id . '_button_select_icon', 'Select Icon')
-																																																																 ->set_html('<a class="button button-primary button-large thickbox select-icon" href="#TB_inline?width=600&height=550&inlineId=modal-svg-" >SELECT ICON</a>')
-																																																																 ->set_conditional_logic(array(
-																																																																 array(
-																																																																 'field' => $id . '_button_type',
-																																																																 'value' => array('page_button', 'post_button', 'services_button', 'custom_button'),
-																																																																 'compare' => 'IN'
-																																																																 )
-																																																																 ))
-																																																																 ->set_width(20)*/
+																																																																		->set_html('<a class="button button-primary button-large thickbox select-icon" href="#TB_inline?width=600&height=550&inlineId=modal-svg-" >SELECT ICON</a>')
+																																																																		->set_conditional_logic(array(
+																																																																		array(
+																																																																		'field' => $id . '_button_type',
+																																																																		'value' => array('page_button', 'post_button', 'services_button', 'custom_button'),
+																																																																		'compare' => 'IN'
+																																																																		)
+																																																																		))
+																																																																		->set_width(20)*/
 
 		);
 
@@ -1306,15 +1306,8 @@ Container::make('theme_options', __('Settings'))
 	->set_page_parent('edit.php')
 	->add_fields(
 		array(
-			Field::make('association', 'suggested_articles', 'Suggested Articles')
-				->set_types(
-					array(
-						array(
-							'type'      => 'post',
-							'post_type' => 'post',
-						)
-					)
-				),
+			Field::make('multiselect', 'suggested_articles', 'Suggested Articles')
+				->set_options($PostMeta->get_posts('post', 'Select Posts')),
 			Field::make('multiselect', 'featured_posts', 'Featured Posts')
 				->set_options($PostMeta->get_posts('post', 'Select Posts')),
 		)
