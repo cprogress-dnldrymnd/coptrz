beafb06eaff96b45da3533f0e93bf36684819424
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-20 16:03:00 +0800

    changes

--- template-parts/header/header-top-bar.php
+++ template-parts/header/header-top-bar.php
@@ -1,4 +1,4 @@
-<div class="top-bar background-black ">
+<div class="top-bar background-black">
     <div class="container large-container">
         <div class="row align-items-center d-none d-lg-flex">
             <div class="col-lg-4 col-sm-6">
@@ -16,7 +16,7 @@
                     <?php dynamic_sidebar('top_bar_right') ?> </div>
             </div>
         </div>
-        <div class="row d-flex d-lg-none">
+        <div class="row d-flex d-lg-none g-0">
             <div class="col-6">
                 <div class="column-holder top-bar-left">
                     <?php dynamic_sidebar('top_bar_left_mobile') ?>
