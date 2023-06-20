52666be54203bbfce8d6a1be49e850e0dd8ccdd7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-06-20 15:52:32 +0800

    Update header-top-bar.php

--- template-parts/header/header-top-bar.php
+++ template-parts/header/header-top-bar.php
@@ -1,6 +1,22 @@
 <div class="top-bar background-black ">
     <div class="container large-container">
-        <div class="row align-items-center d-none d-lg-block">
+        <div class="row align-items-center d-none d-lg-flex">
+            <div class="col-lg-4 col-sm-6">
+                <div class="column-holder top-bar-left">
+                    <?php dynamic_sidebar('top_bar_left') ?>
+                </div>
+            </div>
+            <div class="col-sm-4 text-center d-none d-lg-block">
+                <div class="column-holder top-bar-left">
+                    <?php dynamic_sidebar('top_bar_middle') ?>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 text-end">
+                <div class="column-holder top-bar-right">
+                    <?php dynamic_sidebar('top_bar_right') ?> </div>
+            </div>
+        </div>
+        <div class="row align-items-center d-flex d-lg-none">
             <div class="col-lg-4 col-sm-6">
                 <div class="column-holder top-bar-left">
                     <?php dynamic_sidebar('top_bar_left') ?>
