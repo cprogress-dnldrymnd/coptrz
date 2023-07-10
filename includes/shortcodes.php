4bda95875b502f33a09705f1c14954dfe45a1fcb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-10 19:17:22 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -355,8 +355,10 @@ class Shortcodes
 			)
 		);
 		$brands_arr = array();
-
+		
 		$letters_arr = explode(' ', $letters);
+
+		var_dump($letters_arr);
 		foreach ($terms as $term) {
 			$image = get__term_meta($term->term_id, 'image');
 			$hide_vendor = get__term_meta($term->term_id, 'hide_vendor');
