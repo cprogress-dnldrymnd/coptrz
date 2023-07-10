a9080f8683bb8fdf36607d39fd27fe0f9aafc539
cprogress-dnldrymnd <donald@cprogress.co.uk>
2023-07-10 22:37:46 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -1147,7 +1147,7 @@ Container::make('term_meta', __('Category Properties'))
 	->where('term_taxonomy', '=', 'pa_brands')
 	->add_fields(
 		array(
-			Field::make('textara', 'menu_description', __('Menu Description')),
+			Field::make('textarea', 'menu_description', __('Menu Description')),
 			Field::make('image', 'image', __('Logo')),
 			Field::make('image', 'featured_product_image', 'Featured Product Logo'),
 
